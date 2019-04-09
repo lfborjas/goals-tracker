200849fa50aeef5a673a163ddd7269ac765a60a0
Luis Borjas <luisfborjas@gmail.com>
2019-04-09 16:00:17 -0400

    example of parsing json with aeson, getting it from a simple http req

--- src/YNAB.hs
+++ src/YNAB.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module YNAB ( buildRequest
-            , budgets
+            , budgetsRequest
+--            , allBudgets
             ) where
 
 {-
@@ -11,11 +13,14 @@ https://api.youneedabudget.com/
 
 -}
 
+import Data.Aeson
+import Data.Text as T
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as LC
 import Network.HTTP.Simple
+import GHC.Generics
 
 accessToken :: BC.ByteString
 accessToken = ""
@@ -54,3 +59,61 @@ getResponseBody <$> response
 
 -}
 
+data YNABResponse = YNABResponse
+  { responseData :: YNABData
+  } deriving (Show, Generic)
+
+-- we wrote our own parseJSON to be able to map data -> responseData
+-- since `data` isn't an available name for our record (haskell keyword)
+instance FromJSON YNABResponse where
+  parseJSON (Object v) =
+    YNABResponse <$> v .: "data"
+    -- ignore the rest of the metadata for now
+
+data YNABData = BudgetSet { budgets :: [Budget]}
+  deriving (Show, Generic)
+
+instance FromJSON YNABData -- no need to write our own here
+
+data Budget = Budget
+  { budgetId       :: T.Text
+  , name           :: T.Text
+  , lastModifiedOn :: T.Text
+  , firstMonth     :: T.Text
+  , lastMonth      :: T.Text
+  } deriving (Show, Generic)
+
+instance FromJSON Budget where
+  parseJSON (Object v) =
+    Budget
+    <$> v .: "id"
+    <*> v .: "name"
+    <*> v .: "last_modified_on"
+    <*> v .: "first_month"
+    <*> v .: "last_month"
+
+{-
+
+And here is where I got stuck: there's no escaping the IO monad!
+I was trying to do things in separate functions but httpLBS returns an
+IO (Response LB.ByteString) and once you get in that monad, you either
+go deeper or go nowhere (hence the final type signature)
+
+-}
+
+allBudgets :: IO (Maybe YNABResponse)
+allBudgets = do
+  response <- httpLBS budgetsRequest
+  return $ decode $ getResponseBody response
+
+{-
+
+The above, in a ghci session, shows:
+
+λ> allBudgets
+Just (YNABResponse {responseData = BudgetSet {budgets = [Budget {budgetId = "EGID", name = "EG", lastModifiedOn = "2019-04-02T02:21:41+00:00", firstMonth = "2019-03-01", lastMonth = "2019-04-01"}]}})
+λ> :t allBudgets
+allBudgets :: IO (Maybe YNABResponse)
+λ> 
+
+-}
