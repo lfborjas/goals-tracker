78c47a6a87d6f145ff7ddf79de88a4ba84427cac
Luis Borjas <luisfborjas@gmail.com>
2019-04-14 17:18:56 -0400

    Introduces lenses, responds to user input
    Cleans up imports, too

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE OverloadedLists   #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 module Main where
 
@@ -10,9 +11,7 @@ import           Data.Functor                  (($>))
 import           Data.Text                     (Text)
 import qualified Data.Text                     as Text
 
-import           GI.Gtk                        (Box (..), Button (..),
-                                                Label (..), Orientation (..),
-                                                Window (..), DrawingArea (..), withManagedPtr)
+import qualified GI.Gtk as G
 import           GI.Gtk.Declarative
 import           GI.Gtk.Declarative.App.Simple
 import           Data.Time.LocalTime
@@ -40,19 +39,26 @@ import qualified GI.Cairo as GI.Cairo
 data ButtonEvent = ButtonClicked
 
 clickyButton :: Text -> Widget ButtonEvent
-clickyButton label = widget Button [#label := label, on #clicked ButtonClicked]
+clickyButton label = widget G.Button [#label := label, on #clicked ButtonClicked]
 
 
 type PlotData = [(LocalTime, Double)]
-data State = State { chartData :: PlotData }
+type ProjectionData = (LocalTime, Double)
 
-data Event = Plotted | Plotting | Closed
+data State = State { _chartData :: PlotData, _projectionData :: ProjectionData }
+makeLenses ''State
+
+data Event = Plotted
+           | Plotting
+           | Closed
+           | ProjectingBalance Text.Text
+--           | ProjectingDate    Text.Text
 
 -- from https://github.com/haskell-gi/haskell-gi/wiki/Using-Cairo-with-haskell-gi-generated-bindings
 -- notice that the example expects a Render (), but we actually end up with a Render (PickFn ())
 -- from the plotting lib
 renderWithContext :: GI.Cairo.Context -> C.Render c -> IO c
-renderWithContext ct r = withManagedPtr ct $ \p ->
+renderWithContext ct r = G.withManagedPtr ct $ \p ->
   runReaderT (runRender r) (Cairo (castPtr p))
 
 -- see: https://github.com/timbod7/haskell-chart/wiki/How-to-use-backends
@@ -85,30 +91,32 @@ makeChart cd = toRenderable layout
   
 
 
-updateChart :: PlotData -> GI.Cairo.Context -> DrawingArea -> IO (Bool, Event)
+updateChart :: PlotData -> GI.Cairo.Context -> G.DrawingArea -> IO (Bool, Event)
 updateChart cd ctx canvas = do
   renderWithContext ctx (drawPlot cd)
   return (True, Plotted)
 
 
 
-view' :: State -> AppView Window Event
-view' State {..} =
+view' :: State -> AppView G.Window Event
+view' s =
   bin
-      Window
+      G.Window
       [ #title := "Plot Example"
       , on #deleteEvent (const (True, Closed))
       , #widthRequest := 1200
       , #heightRequest := 900
       ]
     $ container
-        Box
-        [#orientation := OrientationVertical]
-        [ expandingChild $ widget DrawingArea [onM #draw (updateChart chartData)]
+        G.Box
+        [#orientation := G.OrientationVertical]
+        [ expandingChild $ widget G.DrawingArea [onM #draw (updateChart (s ^. chartData))]
         , BoxChild defaultBoxChildProperties $ container
-          Box
-          [#orientation := OrientationHorizontal]
-          [ expandingChild $ clickyButton "Draw Plot" $> Plotting ]
+          G.Box
+          [#orientation := G.OrientationHorizontal]
+          [ expandingChild $ widget G.Label [#label := "Desired Balance" ]
+          ,expandingChild $ widget G.Entry [onM #activate (fmap ProjectingBalance . G.entryGetText)]
+          ,expandingChild $ clickyButton "Draw Plot" $> Plotting ]
         ]
  where
   expandingChild =
@@ -127,21 +135,29 @@ otherData = [
   ,(mkDate 2019 03 01, 20)
   ]
 
+projectData (State _chartData _projectionData) =
+  (State newData _projectionData)
+  where
+    newData = _chartData ++ [_projectionData]
+
 -- from https://github.com/timbod7/haskell-chart/blob/master/chart-tests/tests/Prices.hs
 mkDate d m y =
   LocalTime (fromGregorian (fromIntegral y) m d) midnight
 
 update' :: State -> Event -> Transition State Event
-update' State {..} Plotting = Transition (State otherData) (return Nothing)
+update' s Plotting= Transition (projectData s) (return Nothing)
 update' s          Plotted  = Transition s (return Nothing)
 update' _          Closed   = Exit
+
+update' s (ProjectingBalance b) =
+  Transition (s & projectionData._2 .~ (read (Text.unpack b) :: Double)) (return Nothing)
 
 main :: IO ()
 main = void $ run App
   { view         = view'
   , update       = update'
   , inputs       = []
-  , initialState = State initialData
+  , initialState = State initialData (mkDate 2019 04 01, 0)
   }
 
 {-
